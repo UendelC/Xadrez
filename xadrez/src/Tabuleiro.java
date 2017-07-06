aaa0f9ca2b89b7cf2871a50dc9ac39b3e2e057e5
victorgfb <victorgfb80@gmail.com>
2017-07-06 15:12:15 -0300

    Add files via upload

--- xadrez/src/Tabuleiro.java
+++ xadrez/src/Tabuleiro.java
@@ -1,10 +1,11 @@
+package xadrez;
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author victor
@@ -14,23 +15,57 @@ public class Tabuleiro {
     private final Peca tab[][] = new Peca[8][8];
 
     // implementar parte grafica.
-    public Tabuleiro(Peca[][] tab) {
+    public Tabuleiro() {
 
-        for (Peca[] x : tab) {
-            for (Peca y : x) {
-                y = new Peca();
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                this.tab[x][y] = new Peca();
             }
         }
-            
+
+       //define peoes
+        for (byte x = 0; x < 8; x ++) {
+                tab[1][x] = new Peao(x, (byte) 0);
+                tab[7][x] = new Peao(x, (byte) 7);
+                tab[1][x].setCor(true);
+                tab[7][x].setCor(false);
+          }
+         
         //define torres
-        for(byte x =0 ; x < 8; x+=7){
-            tab[x][0] = new Torre(x,(byte) 0);
-            tab[x][7] = new Torre(x,7);
-           
+        for (byte x = 0; x < 8; x += 7) {
+            tab[0][x] = new Torre((byte) 0, x);
+            tab[7][x] = new Torre((byte) 7, x);
+            tab[0][x].setCor(true);
+            tab[7][x].setCor(false);
         }
-        tab[0][0] = new Torre();
-        tab[0][7] = new Torre();
         
+        //define Cavalo     
+         for (byte x = 0; x < 8; x += 7) {
+            tab[1][x] = new Cavalo(x, (byte) 1);
+            tab[6][x] = new Cavalo(x, (byte) 6);
+            tab[1][x].setCor(true);
+            tab[6][x].setCor(false);
+        }
+         
+         //define Bispo    
+         for (byte x = 0; x < 8; x += 7) {
+            tab[2][x] = new Bispo(x, (byte) 2);
+            tab[5][x] = new Bispo(x, (byte) 5);
+            tab[2][x].setCor(true);
+            tab[5][x].setCor(false);
+        }
+        
+         //define Rei
+         tab[0][4] = new Rei((byte)0, (byte) 4);
+         tab[0][4].setCor(true);
+         tab[7][4] = new Rei((byte)7, (byte) 4);
+         tab[0][4].setCor(false);
+         
+         //define Rainha
+         tab[0][3] = new Rainha((byte)0, (byte) 4);
+         tab[0][3].setCor(true);
+         tab[7][3] = new Rainha((byte)7, (byte) 4);
+         tab[7][3].setCor(false);
         
     }
 
--- xadrez/src/Torre.java
+++ xadrez/src/Torre.java
@@ -1,3 +1,5 @@
+package xadrez;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,10 +12,11 @@
  */
 public class Torre extends Peca {
 
-    Torre(byte x, byte b) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+    public Torre(byte x, byte y) {
+        this.posicao[0] = x;
+        this.posicao[1] = y;
+    }  
+    
      private boolean Mover(byte x, byte y){
          return x== this.posicao[0] ^ y == this.posicao[1];
     }
