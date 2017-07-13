d9911bae378205d9dd19b34630395cf9328da71e
UendelC <uendel.couto@gmail.com>
2017-07-13 14:48:55 -0300

    Update Bispo.java

--- xadrez/src/Bispo.java
+++ xadrez/src/Bispo.java
@@ -7,9 +7,6 @@ public class Bispo extends Peca {
         this.posicao[1] = y;
     } 
     
-    private void Mover(){
-    
-    }
     public boolean caminhoValido(byte finalX, byte finalY){
 	byte xDiff = (byte) Math.abs(finalX - this.posicao[0]);
 	byte yDiff = (byte) Math.abs(finalY - this.posicao[1]);
