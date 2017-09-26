447fcfe5917fb3c78271e670b51dd556f2558d4f
UendelC <uendel.couto@gmail.com>
2017-09-26 15:05:52 -0300

    Update Fachada.java

--- xadrez/src/Fachada.java
+++ xadrez/src/Fachada.java
@@ -79,6 +79,31 @@ public class Fachada {
     }
 
     protected void imprimirTabuleiro(Tabuleiro t) {
+        
+         /* while (true){
+				String arg1="";
+				String arg2="";	
+				String message =JOptionPane.showInputDialog(T);
+				try{
+					arg1=message.substring(0,2);
+					arg2=message.substring(2,4);
+					Posicao k = Posicao.retornarPosicao(arg1);
+					Posicao l = Posicao.retornarPosicao(arg2);
+					Casa a = T.retornarCasa(k);
+					Peca b = a.getPeca();
+					b.moverPeca(k,l,T);
+					
+				}catch(StringIndexOutOfBoundsException si){
+					JOptionPane.showMessageDialog(null,"Entre com o formato correto de casas\nExemplo: a1b5");
+				}
+		*		catch(ArrayIndexOutOfBoundsException ai){
+		*			JOptionPane.showMessageDialog(null,"Entre com o formato correto de casas\nExemplo: a1b5");
+		*		}	
+		*		catch(NullPointerException npe){
+		*		JOptionPane.showMessageDialog(null,"Uma das casas escolhidas é inválida!");
+		*		}
+		*	    
+		}*/
         for (int i = 0; i < 8; i++) {
             System.out.print(8 - i);
             for (int j = 0; j < 8; j++) {
